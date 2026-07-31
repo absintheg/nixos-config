3fb130a13d3c7fcb6b1cc1311ea9f5bb806b194a
absintheg <289622555+absintheg@users.noreply.github.com>
2026-07-31 13:57:41 -0400

    games: add heroic game launcher

--- modules/games.nix
+++ modules/games.nix
@@ -2,6 +2,14 @@
 {
   environment.systemPackages = with pkgs; [
     prismlauncher
+
+    (heroic.override {
+      extraPkgs =
+        pkgs': with pkgs'; [
+          gamescope
+          gamemode
+        ];
+    })
   ];
 
   programs.gamescope.enable = true;
