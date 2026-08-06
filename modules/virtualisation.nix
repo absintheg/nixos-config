665cec1a3c9275f3e1c369030be94f4bf56a9664
absintheg <289622555+absintheg@users.noreply.github.com>
2026-08-06 18:29:56 -0400

    virt: replace qemu for qemu_kvm and enable libvirtd

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -4,9 +4,13 @@
   # Newer kernel versions may need
   virtualisation.waydroid.package = pkgs.waydroid-nftables;
 
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
+
   programs.virt-manager.enable = true;
 
   environment.systemPackages = [
-    pkgs.qemu
+    pkgs.qemu_kvm
+    pkgs.qemu-utils
   ];
 }
