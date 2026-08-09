9c09ce0989c923980726c76cd9a64d6f6aba5d1c
absintheg <289622555+absintheg@users.noreply.github.com>
2026-08-09 17:26:13 -0400

    physical: change to local time

--- modules/physical.nix
+++ modules/physical.nix
@@ -79,7 +79,10 @@
   };
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  # time.timeZone = "America/New_York";
+
+  # For dual booting
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
