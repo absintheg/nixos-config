6124c2a365a01d5b84cbb85402a4b7ee7cc543de
absintheg <289622555+absintheg@users.noreply.github.com>
2026-08-18 15:04:15 +0300

    nvidia: switch drivers to latest and change channels

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs-unstable, ... }:
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
@@ -33,6 +33,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = pkgs-unstable.linuxPackages.nvidiaPackages.latest;
   };
 }
