360676d3f921813d1099587270f8ffa03cd9dc29
absintheg <289622555+absintheg@users.noreply.github.com>
2026-08-21 13:42:44 +0300

    obs: disable cuda support

--- modules/obs.nix
+++ modules/obs.nix
@@ -1,21 +1,16 @@
-{ pkgs, ... }:
+{
+  # pkgs,
+  ...
+}:
 {
   programs.obs-studio = {
     enable = true;
 
-    # optional Nvidia hardware acceleration
-    package = (
-      pkgs.obs-studio.override {
-        cudaSupport = true;
-      }
-    );
-
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-      obs-gstreamer
-      obs-vkcapture
-    ];
+	# Errors after switching to unstable
+    # package = (
+    #   pkgs.obs-studio.override {
+    #     cudaSupport = true;
+    #   }
+    # );
   };
 }
