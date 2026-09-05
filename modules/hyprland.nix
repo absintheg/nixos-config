d4e90bc1fa97abf1ffbb6ff85e93ec1cabd167bd
absintheg <289622555+absintheg@users.noreply.github.com>
2026-09-06 02:21:59 +0300

    make desktop environments optional

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,34 +1,43 @@
 {
   pkgs,
   pkgs-unstable,
+  config,
+  lib,
   ...
 }:
+let
+  cfg = config.my.hyprland;
+in
 {
-  programs.hyprland = {
-    enable = true;
-    package = pkgs-unstable.hyprland;
-    withUWSM = true;
-    xwayland.enable = true;
+  options.my.hyprland.enable = lib.mkEnableOption "Hyprland";
+
+  config = lib.mkIf cfg.enable {
+    programs.hyprland = {
+      enable = true;
+      package = pkgs-unstable.hyprland;
+      withUWSM = true;
+      xwayland.enable = true;
+    };
+
+    environment.systemPackages =
+      with pkgs;
+      [
+        noctalia-shell
+        wl-clipboard
+      ]
+      ++ (with pkgs.kdePackages; [
+        dolphin
+        ark
+        kate
+        okular
+        kolourpaint
+        kcalc
+        kclock
+        kcharselect
+        discover
+        plasma-systemmonitor
+      ]);
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
   };
-
-  environment.systemPackages =
-    with pkgs;
-    [
-      noctalia-shell
-      wl-clipboard
-    ]
-    ++ (with pkgs.kdePackages; [
-      dolphin
-      ark
-      kate
-      okular
-      kolourpaint
-      kcalc
-      kclock
-      kcharselect
-      discover
-      plasma-systemmonitor
-    ]);
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
