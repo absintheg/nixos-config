862e089c9e9ec62e0c43d361d67b7e2a8a4e59a7
absintheg <289622555+absintheg@users.noreply.github.com>
2026-09-06 02:21:59 +0300

    make desktop environments optional

--- modules/kde.nix
+++ modules/kde.nix
@@ -1,25 +1,37 @@
-{ pkgs, ... }:
 {
-  services = {
-    desktopManager.plasma6.enable = true;
-    displayManager.sddm.enable = true;
-    displayManager.sddm.wayland.enable = true;
-    xserver.enable = true;
-  };
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.my.plasma;
+in
+{
+  options.my.plasma.enable = lib.mkEnableOption "KDE Plasma";
 
-  environment.systemPackages =
-    with pkgs;
-    [
-      wl-clipboard
-    ]
-    ++ (with pkgs.kdePackages; [
-      discover # Optional: Software center for Flatpaks/firmware updates
-      kcalc # Calculator
-      kcharselect # Character map
-      kclock # Clock app
-      kcolorchooser # Color picker
-      kolourpaint # Simple paint program
-      ksystemlog # System log viewer
-      sddm-kcm # SDDM configuration module
-    ]);
+  config = lib.mkIf cfg.enable {
+    services = {
+      desktopManager.plasma6.enable = true;
+      displayManager.sddm.enable = true;
+      displayManager.sddm.wayland.enable = true;
+      xserver.enable = true;
+    };
+
+    environment.systemPackages =
+      with pkgs;
+      [
+        wl-clipboard
+      ]
+      ++ (with pkgs.kdePackages; [
+        discover # Optional: Software center for Flatpaks/firmware updates
+        kcalc # Calculator
+        kcharselect # Character map
+        kclock # Clock app
+        kcolorchooser # Color picker
+        kolourpaint # Simple paint program
+        ksystemlog # System log viewer
+        sddm-kcm # SDDM configuration module
+      ]);
+  };
 }
